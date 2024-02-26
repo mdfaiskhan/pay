6b02f0671a0eaca14b99ba821438a928a9d493f9
Mohammed Faisal Khan <76160659+mdfaiskhan@users.noreply.github.com>
2024-02-26 22:47:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// index.js
+let faisal=123;
+let yasir="boss";
+// faisal=yasir;
+// console.log(1/0);
+// console.log(faisal);
+// console.log(faisal+yasir);
+console.log("Hello, World!");
+console.log(9007199254740991 + 1+1); // max number
+let name = "John";
+const birthday = new Date;
+console.log(birthday);
+
+// embed a variable
+console.log( `Hello, ${name}!` ); // Hello, John!
+console.log(`bro ${2+3}`);
+let greater = 4>1;
+console.log( greater);
+let aa=null;
+console.log( aa);
+let moen;
+console.log(moen)
+console.log(typeof greater);
+
+
+// embed an expression
+console.log( `the result is ${1 + 2}` ); // the result is 3
+
+
+
